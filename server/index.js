546a85e9a294781e3d550c9f7cae981773d7c260
mdshahco <mdshahcoding.gmail.com>
2023-10-10 20:02:36 +0600

    middelware and a function to handle possible errors

--- server/index.js
+++ server/index.js
@@ -15,14 +15,23 @@ app.use(express.json());
 
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter);
-
-
-
-app.get('/test', (req, res)=>{
-    res.json({
-        message: 'hello world',
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
     });
 });
+
+
+
+// app.get('/test', (req, res)=>{
+//     res.json({
+//         message: 'hello world',
+//     });
+// });
 
 
 
