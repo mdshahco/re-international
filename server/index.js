32a66322d1f68922ea4c3acbce4c5214cd8b22cf
mdshahco <mdshahcoding.gmail.com>
2023-10-10 19:43:44 +0600

    sign up server route created

--- server/index.js
+++ server/index.js
@@ -1,26 +1,41 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-
 import connectDB from './mongodb/connect.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+
+
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send({ message: 'Hello world'});
+app.use("/server/user", userRouter);
+app.use("/server/auth", authRouter);
 
 
-})
+
+app.get('/test', (req, res)=>{
+    res.json({
+        message: 'hello world',
+    });
+});
+
+
+
+
+
+
+
 
 const startServer = async()=>{
     try{
         connectDB(process.env.MONGODB_URL);
 
-        app.listen(8081,()=> console.log('Server started on port http://locathost:8081'));
+        app.listen(3000,()=> console.log('Server started on port http://locathost:3000'));
 
     } catch(error){
         console.log(error);
@@ -28,3 +43,4 @@ const startServer = async()=>{
 }
 
 startServer();
+
